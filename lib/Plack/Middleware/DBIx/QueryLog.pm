d085881b3354082c0e46de8730cca5164e5b0d58
dameninngenn <dameninngenn.owata@gmail.com>
2012-08-06 15:55:29 +0900

    initial commit

---
+++ lib/Plack/Middleware/DBIx/QueryLog.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::DBIx::QueryLog;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::DBIx::QueryLog -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::DBIx::QueryLog;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::DBIx::QueryLog is
+
+=head1 AUTHOR
+
+dameninngenn E<lt>dameninngenn.owata {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
